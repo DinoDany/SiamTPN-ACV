e769edbd0408df803eceb615cfb440c0625b8805
DannyTj <adanielatejedao@gmail.com>
2024-06-14 07:51:14 -0700

    Hanigng the template size

--- tpn/running_inference.py
+++ tpn/running_inference.py
@@ -27,6 +27,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 transform2 = transforms.Compose([
+    transforms.Resize(80),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
